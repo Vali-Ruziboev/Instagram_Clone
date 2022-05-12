f68eb9384a5b3d58a5cfb545bbcbfc9ace62edfd
Vali <valiruziboyev212@gmail.com>
2022-05-12 17:10:53 +0500

    sing in page

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,12 @@ import {
     MenuIcon,
     HomeIcon,
 } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Header = () => {
+    const router = useRouter()
+    const {data: session} = useSession();
     return (  
         <div className="shadow-sm border-b bg-white sticky top-0 z-50 pb-2">
         
@@ -17,14 +21,14 @@ const Header = () => {
 
 
                 {/* Left */}
-                    <div className="relative hidden lg:inline-grid w-24 cursor-pointer" >
+                    <div onClick={()=>router.push('/')} className="relative hidden lg:inline-grid w-24 cursor-pointer" >
                         <Image 
                             src='https://links.papareact.com/ocw'
                             layout='fill'
                             objectFit="contain"
                         />
                     </div>
-                    <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+                    <div onClick={()=>router.push('/')} className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
                         <Image 
                             src='https://links.papareact.com/jjm'
                             layout='fill'
@@ -46,18 +50,24 @@ const Header = () => {
 
                 {/* Right */}
                 <div className="flex items-center justify-end space-x-4">
-                    <HomeIcon className="navBtn" />
+                    <HomeIcon onClick={()=>router.push('/')} className="navBtn" />
                     <MenuIcon className="h-6 md:hidden" />
+                    {session? (
+                        <>
+                            <div className="relative navBtn">
+                                <PaperAirplaneIcon className="navBtn rotate-45" />
+                                <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">4</div>
+                            </div>
 
-                    <div className="relative navBtn">
-                        <PaperAirplaneIcon className="navBtn rotate-45" />
-                        <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">4</div>
-                    </div>
+                            <PlusCircleIcon className="navBtn" />
+                            <UserGroupIcon className="navBtn" /> 
+                            <HeartIcon className="navBtn" />
+                            <img onClick={signOut} src={session.user.image} alt="profile pic"  className="h-10 rounded-full cursor-pointer"/> 
+                        </>
 
-                    <PlusCircleIcon className="navBtn" />
-                    <UserGroupIcon className="navBtn" /> 
-                    <HeartIcon className="navBtn" />
-                    <img src="https://avatars.githubusercontent.com/u/89704608?v=4" alt="profile pic"  className="h-10 rounded-full"/> 
+                    ):(
+                        <button onClick={signIn}>Sign In</button>
+                    )}
                 </div>
             </div>
 
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,14 +1,17 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 const MiniProfile = () => {
+    const {data: session} = useSession();
     return (  
         <div className="flex items-center justify-between mt-14 ml-10">
-            <img className="w-16 h-16 rounded-full border p=[2px]" src="https://avatars.githubusercontent.com/u/89704608?v=4" alt="img" />
+            <img className="w-16 h-16 rounded-full border p=[2px]" src={session.user.image} alt="img" />
 
             <div className="flex-1 mx-4">
-                <h2 className='font-bold'>vali_ruziboev</h2>
+                <h2 className='font-bold'>{session?.user?.username}</h2>
                 <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
             </div>
 
-            <button className="text-blue-400 text-sm font-semibold" >Sign out</button>
+            <button onClick={signOut} className="text-blue-400 text-sm font-semibold" >Sign out</button>
         </div>
     );
 }
